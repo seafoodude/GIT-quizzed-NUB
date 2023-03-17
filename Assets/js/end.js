8ceada6f448df9c73b392bf0739543cf6619bb54
seafoodude <michaelhuang777@gmail.com>
2023-03-16 18:09:13 -0700

    fixed the save button

--- Assets/js/end.js
+++ Assets/js/end.js
@@ -30,5 +30,5 @@ saveHighScore = e => {
     highScores.splice(MAX_HIGH_SCORES);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('./highscores.html');
 }
